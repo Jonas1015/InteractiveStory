d1fc61b91242e802ecd5a0ff1f906ad0fd61a7a9
jonas1015 <jonasgsmart1015@gmail.com>
2023-06-16 12:53:26 +0300

    Add data in first page

--- app/src/main/java/com/nocturnalcodes/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/nocturnalcodes/interactivestory/ui/StoryActivity.java
@@ -1,25 +1,58 @@
 package com.nocturnalcodes.interactivestory.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.nocturnalcodes.interactivestory.R;
 import com.nocturnalcodes.interactivestory.model.Page;
+import com.nocturnalcodes.interactivestory.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
     public static final String TAG = StoryActivity.class.getSimpleName();
+    private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
+    private String name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
 
+        storyImageView = findViewById(R.id.storyImageView);
+        storyTextView = findViewById(R.id.storyTextView);
+        choice1Button = findViewById(R.id.choice1Button);
+        choice2Button = findViewById(R.id.choice2Button);
+
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        name = intent.getStringExtra(getString(R.string.key_name));
         if(name == null || name.isEmpty()){
             name = "Friend";
         }
+        story = new Story();
+        loadPage(0);
+    }
+
+    private void loadPage(int pageNumber) {
+        Page page = story.getPage(pageNumber);
+
+        Drawable image = ContextCompat.getDrawable(this, page.getImageId());
+        storyImageView.setImageDrawable(image);
+
+        String pageText = getString(page.getTextId());
+        // Add name if placeholder included, Won't add if not
+        pageText = String.format(pageText, name);
+        storyTextView.setText(pageText);
+        choice1Button.setText(page.getChoice1().getTextId());
+        choice2Button.setText(page.getChoice2().getTextId());
     }
 }
